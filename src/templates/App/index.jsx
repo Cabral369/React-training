8ae1d3cc25f7c4f696620400799e7349ab1ae165
Cabral369 <nicolas.cabral.carvalho@gmail.com>
2025-01-13 21:21:01 -0300

    Primeiro commit

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div style={{ backgroundColor: 'lightblue', padding: '10px' }}>
+        <h1>Seja bem vindo</h1>
+        <p>Esse é um exemplo de um projeto</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
